bd7a96f78aea72be8ac061ffc92fac449796391c
Nigel Russell <nigel@renturapp.com>
2024-03-26 10:39:31 +0000

    Change route to use sanctum for authentication

--- routes/devices_routes.php
+++ routes/devices_routes.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Sfolador\Devices\Controllers\DeviceController;
 
-Route::middleware('api')->prefix('api')->group(function () {
+Route::middleware('auth:sanctum')->prefix('api')->group(function () {
     Route::prefix('devices')->group(function () {
         Route::post('/attach', [DeviceController::class, 'attach'])->name('devices:attach');
     });
